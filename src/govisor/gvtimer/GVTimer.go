86f7eb4a1cf4dd172091c0ee6413ca1be95f3641
Mohan Das Katragadda <mohan.das142@gmail.com>
2013-10-13 03:52:16 -0400

    Timer Related Operations.Need to add priority Queue.

---
+++ src/govisor/gvtimer/GVTimer.go
@@ -0,0 +1,14 @@
+package gvtimer
+
+import (
+
+)
+//MAX_TIMEOUT long = 5000
+
+type GVTimer struct{
+	max_timeout int64
+	min_timeout int64
+}
+func NewGVTimer() *GVTimer {
+	return &GVTimer{max_timeout:5000, min_timeout:1}
+}
