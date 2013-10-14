f2f4f61a07011db579a75cbe3cc74dd71070af96
Mohan Das Katragadda <mohan.das142@gmail.com>
2013-10-13 20:19:33 -0400

    pq changes

--- src/govisor/gvtimer/GVTimer.go
+++ src/govisor/gvtimer/GVTimer.go
@@ -12,3 +12,25 @@ type GVTimer struct{
 func NewGVTimer() *GVTimer {
 	return &GVTimer{max_timeout:5000, min_timeout:1}
 }	
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	gvtimerevent := x.(*GVTimerEvent)
+	gvtimerevent.index = n
+	*pq = append(*pq, gvtimerevent)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	gvtimerevent := old[n-1]
+	gvtimerevent.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return gvtimerevent
+}
