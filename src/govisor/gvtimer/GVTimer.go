a9e6e78c6b3babde7cb12aba02485194e55b3357
Mohan Das Katragadda <mohan.das142@gmail.com>
2013-10-13 20:22:01 -0400

    Moving PQ code to Utils File

--- src/govisor/gvtimer/GVTimer.go
+++ src/govisor/gvtimer/GVTimer.go
@@ -13,32 +13,3 @@ func NewGVTimer() *GVTimer {
 	return &GVTimer{max_timeout:5000, min_timeout:1}
 }	
 
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	gvtimerevent := x.(*GVTimerEvent)
-	gvtimerevent.index = n
-	*pq = append(*pq, gvtimerevent)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	gvtimerevent := old[n-1]
-	gvtimerevent.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return gvtimerevent
-}
-
-// update modifies the priority and value of an GVTimerEvent in the queue.
-func (pq *PriorityQueue) update(gvtimerevent *GVTimerEvent, value string, priority int) {
-	heap.Remove(pq, gvtimerevent.index)
-	gvtimerevent.value = value
-	gvtimerevent.priority = priority
-	heap.Push(pq, gvtimerevent)
-}
