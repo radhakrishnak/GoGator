668be6f1a50b909b7d3c0ecab770f2e0bfbe5361
Vishwanath Vuradi <vvuradi@ufl.edu>
2013-10-14 20:07:14 -0400

    Added constructor, get and set methods for missSendLength

--- src/govisor/slicer/GVSlicer.go
+++ src/govisor/slicer/GVSlicer.go
@@ -4,6 +4,7 @@ import (
 
 )
 const MessagesPerRead int = 50
+const max_allowed_buffer_ids = 256
 
 type GVSlicer struct{
 	sliceName string
@@ -11,17 +12,34 @@ type GVSlicer struct{
 	hostname string
 	reconnectSeconds int
 	maxReconnectSeconds int //should be initialised
-	port int
+	port int // tcp port of controller
 	isConnected bool
 	connectCount int //init to 0
-	missSendLength int8
+	missSendLength int
 	allowAllPorts bool
 	isShutdown bool
 	lldpOptIn bool // default to true
 	floodPerms bool
 	allowedPorts map[int8]bool
 	reconnectEventScheduled bool //default to false
-	
+	fmlimit int
+}
+
+func NewGVSlicer(sName string) *GVSlicer{
+	sl := GVSlicer{sliceName:sName}
+	sl.missSendLength = 128
+	sl.fmlimit = -1
+	sl.maxReconnectSeconds = 15
+	sl.lldpOptIn = true
+	return &sl
+}
+
+func (gSlicer *GVSlicer)SetMissSendLength(val int){
+	gSlicer.missSendLength = val
+} 
+
+func (gSlicer GVSlicer)GetMissSendLength() int{
+	return gSlicer.missSendLength
 }
 
 func (gSlicer GVSlicer)updatePortList(){
